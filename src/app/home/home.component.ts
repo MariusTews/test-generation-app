058fd45b574bcd7911a82320ca360b08ebfe3fb0
Marius Tews <uk069544@student.uni-kassel.de>
2024-07-10 19:35:32 +0200

    wip: output controls

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -19,6 +19,8 @@ export class HomeComponent {
   genAI = new GoogleGenerativeAI(environment.API_KEY);
   model = this.genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
   output = '';
+  outputText = '';
+  outputReceived = false;
 
   form = new FormControl('', [Validators.required]);
 
@@ -37,13 +39,30 @@ export class HomeComponent {
   }
 
   async run() {
+    this.outputReceived = false;
     const prompt =
       'Write an Angular Test with Playwright. Return only the pure code. Here is my code: ' +
       this.form.value;
-    this.output = 'Generating code...';
+    this.outputText = 'Generating code...';
     const result = await this.model.generateContent(prompt);
     const response = await result.response;
-    this.output = response.text();
+    const lines = response.text().split('\n');
+    this.output = lines.slice(1, lines.length - 1).join('\n');
+    // this.output = 'import { Component } from "@angular/core"\n' +
+    //     '@Component({\n' +
+    //     '   selector: "app-root",\n' +
+    //     '   templateUrl: "./app.component.html",\n' +
+    //     '   styleUrls: ["./app.component.css"]\n' +
+    //     '})\n' +
+    //     'export class AppComponent {\n' +
+    //     '   title = "test-generation-app";\n' +
+    //     '}'
+    this.outputReceived = true;
+    this.outputText = '';
+  }
+
+  showOutput() {
+    this.outputText = this.output;
   }
 
   private _injector = inject(Injector);
