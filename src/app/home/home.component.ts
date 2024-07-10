8956e8049113741f095b387a938be04080d4b7fa
Marius Tews <uk069544@student.uni-kassel.de>
2024-07-10 17:52:12 +0200

    feat: simple unit test generation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import { CdkTextareaAutosize } from '@angular/cdk/text-field';
+import {
+  afterNextRender,
+  Component,
+  inject,
+  Injector,
+  ViewChild,
+} from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 import { GoogleGenerativeAI } from '@google/generative-ai';
 import { environment } from 'src/environments/environment';
 
@@ -10,12 +18,37 @@ import { environment } from 'src/environments/environment';
 export class HomeComponent {
   genAI = new GoogleGenerativeAI(environment.API_KEY);
   model = this.genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
-  output = '';
+  output =
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\n' +
+    'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\n' +
+    'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.\n' +
+    'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+
+  form = new FormControl('', [Validators.required]);
 
   async run() {
-    const prompt = 'Write a simple angular e2e test with Playwright.';
+    const prompt =
+      'Write an Angular Unit Test with Playwright. Return only the pure code. Here is my code: ' +
+      this.form.value;
     const result = await this.model.generateContent(prompt);
     const response = await result.response;
     this.output = response.text();
+  }
+
+  private _injector = inject(Injector);
+
+  @ViewChild('autosize')
+  autosize!: CdkTextareaAutosize;
+
+  triggerResize() {
+    // Wait for content to render, then trigger textarea resize.
+    afterNextRender(
+      () => {
+        this.autosize.resizeToFitContent(true);
+      },
+      {
+        injector: this._injector,
+      }
+    );
   }
 }
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,9 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { AppRoutingModule } from '../app-routing.module';
+import { TextFieldModule } from '@angular/cdk/text-field';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, AppRoutingModule],
+  imports: [
+    CommonModule,
+    AppRoutingModule,
+    TextFieldModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
 })
 export class HomeModule {}
